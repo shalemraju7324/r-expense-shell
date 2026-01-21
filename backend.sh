fdedd5f6f9278b455f2d9d5cda0c62c5030f9183
shalemraju <shalemrajudevops@gamil.com>
2026-01-21 15:32:55 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -66,7 +66,7 @@ VALIDATE $? "unzip backend"
 npm install &>>$LOG_FILE_NAME
 VALIDATE $? "Installing dependencies"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/r-expense-shell/backend.service /etc/systemd/system/backend.service
 
 # Prepare MySQL Schema
 
